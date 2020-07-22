a7add0ab140eeb60ae29900bf0aed0d84db7605a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-22 22:26:26 +0300

    Adds films card amount

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@
 const pageHeaderElement = document.querySelector(`.header`);
 const pageMainElement = document.querySelector(`.main`);
 const pageFooterElement = document.querySelector(`.footer`);
+
+const FILM_CARDS_AMOUNT = 5;
+const FILM_RATED_CARDS_AMOUNT = 2;
+const FILM_COMMENTED_CARDS_AMOUNT = 2;

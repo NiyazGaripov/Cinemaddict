49fb1b893d4ffd117c05db2fa789cff55181206e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-22 22:27:06 +0300

    Creates function generate markup profile

--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@ const pageFooterElement = document.querySelector(`.footer`);
 const FILM_CARDS_AMOUNT = 5;
 const FILM_RATED_CARDS_AMOUNT = 2;
 const FILM_COMMENTED_CARDS_AMOUNT = 2;
+
+const createProfileComponent = () => {
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`
+  );
+};

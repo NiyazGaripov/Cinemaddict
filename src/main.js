3fc7b81f9a620dde6a5e93badb64968f3eefbc64
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-22 22:24:17 +0300

    Finds DOM elements

---
+++ src/main.js
@@ -0,0 +1,4 @@
+'use strict';
+const pageHeaderElement = document.querySelector(`.header`);
+const pageMainElement = document.querySelector(`.main`);
+const pageFooterElement = document.querySelector(`.footer`);

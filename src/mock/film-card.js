0ead3ffb795c4c571483fed932fab7aa84560b44
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-25 09:11:23 +0300

    Creates film-card module in mock directory and func generate film card

---
+++ src/mock/film-card.js
@@ -0,0 +1,12 @@
+const generateFilmCard = () => {
+  return {
+    poster: `./images/posters/the-dance-of-life.jpg`,
+    name: `The Dance of Life`,
+    rating: `8.3`,
+    releaseYear: `1929`,
+    duration: `1h 55m`,
+    genre: `Musical`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    comments: [{}],
+  };
+};

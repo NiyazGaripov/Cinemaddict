4cac0bfc6fa1c8dcb0c5c2b773036f6b97f98a1a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-26 18:32:36 +0300

    Creates func generate comments

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -6,3 +6,9 @@ const generateComment = () => {
     date: `2019/12/31 23:59`,
   };
 };
+
+export const generateComments = (amount) => {
+  return new Array(amount)
+    .fill(``)
+    .map(generateComment);
+};

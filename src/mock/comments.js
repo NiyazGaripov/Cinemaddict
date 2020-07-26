269225dc30b4d086570894e81423515abe236b32
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-26 18:15:39 +0300

    Creates comments module and func generate comment

---
+++ src/mock/comments.js
@@ -0,0 +1,8 @@
+const generateComment = () => {
+  return {
+    text: `Interesting setting and a good cast`,
+    emoji: `./images/emoji/smile.png`,
+    author: `Tim Macoveev`,
+    date: `2019/12/31 23:59`,
+  };
+};

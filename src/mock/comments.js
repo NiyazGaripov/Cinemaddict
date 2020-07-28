546664fa42af5cfe7435cb511326b946d4660027
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-28 13:39:10 +0300

    Creates comment dates

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -19,6 +19,13 @@ const COMMENT_AUTHORS = [
   `John Macoveev`,
 ];
 
+const COMMENT_DATES = [
+  `2019/12/31 23:59`,
+  `2020/01/01 00:01`,
+  `2020/02/05 10:01`,
+  `2020/03/21 15:30`,
+];
+
 const generateComment = () => {
   return {
     text: `Interesting setting and a good cast`,

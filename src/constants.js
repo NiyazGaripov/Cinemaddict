36866bfed1f58d2f4ae4ef08aae4f0aed65e9328
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-11 19:42:19 +0300

    Creates module constants and transfers constants from module comments

---
+++ src/constants.js
@@ -0,0 +1,66 @@
+export const TEXT_COMMENTS = [
+  `Interesting setting and a good cast`,
+  `Booooooooooring`,
+  `Very very old. Meh`,
+  `Almost two hours? Seriously?`
+];
+
+export const EMOJIS = [
+  `smile`,
+  `sleeping`,
+  `puke`,
+  `angry`,
+];
+
+export const COMMENT_AUTHORS = [
+  `Tim Macoveev`,
+  `John Doe`,
+  `Tim Doe`,
+  `John Macoveev`,
+];
+
+export const COMMENT_DATES = [
+  `2019/12/31 23:59`,
+  `2020/01/01 00:01`,
+  `2020/02/05 10:01`,
+  `2020/03/21 15:30`,
+];
+
+export const POSTERS = [
+  `the-dance-of-life`,
+  `sagebrush-trail`,
+  `the-man-with-the-golden-arm`,
+  `santa-claus-conquers-the-martians`,
+  `popeye-meets-sinbad`,
+  `the-great-flamarion`,
+  `made-for-each-other`,
+];
+
+export const FILM_TITLES = [
+  `The Dance of Life`,
+  `Sagebrush Trail`,
+  `The Man with the Golden Arm`,
+  `Santa Claus Conquers the Martians`,
+  `Popeye the Sailor Meets Sindbad the Sailor`,
+  `The Great Flamarion`,
+  `Made for Each Other`,
+];
+
+export const GENRE = [
+  `Musical`,
+  `Western`,
+  `Drama`,
+  `Comedy`,
+  `Cartoon`,
+  `Mystery`,
+];
+
+export const DESCRIPTION_FILMS = [
+  `Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…`,
+  `Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brant's narrow escap…`,
+  `Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…`,
+  `The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti…`,
+  `In this short, Sindbad the Sailor (presumably Bluto playing a "role") proclaims himself, in song, to be the greatest sailor, adventurer and…`,
+  `The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Grea…`,
+  `John Mason (James Stewart) is a young, somewhat timid attorney in New York City. He has been doing his job well, and he has a chance of bei…`,
+];

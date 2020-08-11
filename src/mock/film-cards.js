8c6add013582cf9f01c7a9900287e2a64113aee4
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-11 19:47:47 +0300

    Adds constants

--- src/mock/film-cards.js
+++ src/mock/film-cards.js
@@ -1,7 +1,12 @@
 import {POSTERS, FILM_TITLES, DURATION, GENRE, DESCRIPTION_FILMS} from './../constants.js';
-import {getRandomIntegerNumber} from './../utils/common.js';
+import {getRandomIntegerNumber, getRandomArrayItem} from './../utils/common.js';
 import {generateComments} from './comments.js';
 
+const DATE = new Date();
+const MIN_RATING = 0;
+const MAX_RATING = 9;
+const MIN_RELEASE_YEAR = 1920;
+const MAX_RELEASE_YEAR = DATE.getFullYear();
 const MIN_COMMENTS_AMOUNT = 0;
 const MAX_COMMENTS_AMOUNT = 5;
 const commentsAmount = getRandomIntegerNumber(MIN_COMMENTS_AMOUNT, MAX_COMMENTS_AMOUNT);

b3553de9d456a2e237d3aca85a3366435186592c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-18 19:21:03 +0300

    Adds random user rank

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,9 +2,11 @@ import {PROFILE} from './../constants.js';
 import {getRandomArrayItem} from './../utils/common.js';
 
 export const createProfileComponent = () => {
+  const userRank = getRandomArrayItem(PROFILE);
+
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${userRank}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );

4ffae3298e9bba2f067155d18d5ee6b450d1b81d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-19 23:35:28 +0300

    Refactors creation navigational elements

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -10,14 +10,13 @@ const createNavigationItemComponent = (item, isActive, hasCount) => {
   );
 };
 
-export const createNavigationComponent = () => {
+export const createNavigationComponent = (list) => {
+  const createNavigationList = list.map((it, i) => createNavigationItemComponent(it, i === 0, i !== 0)).join(`\n`);
+
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+        ${createNavigationList}
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`

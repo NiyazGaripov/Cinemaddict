75d3e1d9e5564bdfaab037217c6b203040eb5bb8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-23 21:00:22 +0300

    Imports generateSortList and passes it as parameter

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import {createFilmsListComponent} from './components/film-list.js';
 import {createFilmStatisticsComponent} from './components/film-statistics.js';
 import {createFilmDetailsComponent} from './components/film-details.js';
 import {generateNavigationList} from './mock/nav-list.js';
+import {generateSortList} from './mock/sort-list.js';
 
 const pageHeaderElement = document.querySelector(`.header`);
 const pageMainElement = document.querySelector(`.main`);
 const pageFooterElement = document.querySelector(`.footer`);
 const navList = generateNavigationList();
-
+const sortList = generateSortList();
+console.log(sortList)
 renderComponent(pageHeaderElement, createProfileComponent());
 renderComponent(pageMainElement, createNavigationComponent(navList));
-renderComponent(pageMainElement, createSortComponent());
+renderComponent(pageMainElement, createSortComponent(sortList));
 renderComponent(pageMainElement, createFilmsListComponent());
 renderComponent(pageFooterElement, createFilmStatisticsComponent());
 renderComponent(document.body, createFilmDetailsComponent());

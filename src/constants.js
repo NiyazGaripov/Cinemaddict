24224f21b769106fc2597b7aff45067366d45236
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-28 10:56:45 +0300

    Removes comment dates

--- src/constants.js
+++ src/constants.js
@@ -19,13 +19,6 @@ export const COMMENT_AUTHORS = [
   `John Macoveev`,
 ];
 
-export const COMMENT_DATES = [
-  `2019/12/31 23:59`,
-  `2020/01/01 00:01`,
-  `2020/02/05 10:01`,
-  `2020/03/21 15:30`,
-];
-
 export const POSTERS = [
   `the-dance-of-life.jpg`,
   `sagebrush-trail.jpg`,

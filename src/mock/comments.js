e690f5680b7fc04114c455081f75cd255ea56111
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-28 10:56:03 +0300

    Refactors date

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -1,12 +1,12 @@
-import {TEXT_COMMENTS, EMOJIS, COMMENT_AUTHORS, COMMENT_DATES} from './../constants.js';
-import {getRandomArrayItem} from './../utils/common.js';
+import {TEXT_COMMENTS, EMOJIS, COMMENT_AUTHORS} from './../constants.js';
+import {getRandomArrayItem, getRandomDate} from './../utils/common.js';
 
 const generateComment = () => {
   return {
     text: getRandomArrayItem(TEXT_COMMENTS),
     emoji: getRandomArrayItem(EMOJIS),
     author: getRandomArrayItem(COMMENT_AUTHORS),
-    date: getRandomArrayItem(COMMENT_DATES),
+    date: getRandomDate(),
   };
 };
 

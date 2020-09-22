be1bb72721aa3564b0bb29f0207f0a717ff96681
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-22 22:33:22 +0300

    Imports create element and creates class film statistics

--- src/components/film-statistics.js
+++ src/components/film-statistics.js
@@ -1,7 +1,31 @@
-export const createFilmStatisticsComponent = () => {
+import {createElement} from './../utils/common.js';
+
+const createFilmStatisticsComponent = () => {
   return (
     `<section class="footer__statistics">
       <p>130 291 movies inside</p>
     </section>`
   );
 };
+
+class FilmStatistics {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmStatisticsComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}

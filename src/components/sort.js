da1cb898704da8711989d6224a146a30a185023b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-23 20:05:30 +0300

    Renames module, imports create element and creates class sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,3 +1,5 @@
+import {createElement} from './../utils/common.js';
+
 const createSortItemComponent = (item, isActive) => {
   const {title} = item;
   const activeClass = isActive ? `sort__button--active` : ``;
@@ -9,7 +11,7 @@ const createSortItemComponent = (item, isActive) => {
   );
 };
 
-export const createSortComponent = (list) => {
+const createSortComponent = (list) => {
   const createSortList = list.map((it, i) => createSortItemComponent(it, i === 0)).join(`\n`);
 
   return (
@@ -18,3 +20,26 @@ export const createSortComponent = (list) => {
     </ul>`
   );
 };
+
+export class Sort {
+  constructor(list) {
+    this._list = list;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortComponent(this._list);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}

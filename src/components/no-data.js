88ec3c6f3785eeb1d7e21d04cf428b34f11ddbf3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-27 22:28:33 +0300

    Creates func createNoDataComponent

--- src/components/no-data.js
+++ src/components/no-data.js
@@ -1,1 +1,7 @@
 import {createElement} from './../utils/common.js';
+
+const createNoDataComponent = () => {
+  return (
+    `<h2 class="films-list__title">There are no movies in our database</h2>`
+  );
+};

7b6a89b9b6e73de4eef17d80f66e5406715796f1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-27 22:29:03 +0300

    Creates class no data

--- src/components/no-data.js
+++ src/components/no-data.js
@@ -5,3 +5,25 @@ const createNoDataComponent = () => {
     `<h2 class="films-list__title">There are no movies in our database</h2>`
   );
 };
+
+export class NoData {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoDataComponent();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}

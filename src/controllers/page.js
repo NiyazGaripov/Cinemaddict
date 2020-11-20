2231405da36244fd5a4cc3d97dc48299fa248707
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-20 23:53:31 +0300

    Adds setSortTypeChangeHandler

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -86,6 +86,15 @@ export class PageController {
 
     renderFilmCards(films.slice(BEGIN_INDEX, showingFilmCards), filmsListContainer);
     renderShowMoreButton();
+
+    this._sortComponent.setSortTypeChangeHandler(() => {
+      showingFilmCards += FILM_CARDS_AMOUNT_LOAD_MORE;
+
+      filmsListContainer.innerHTML = ``;
+
+      renderFilmCards(films.slice(BEGIN_INDEX, showingFilmCards), filmsListContainer);
+      renderShowMoreButton();
+    });
   }
 
   renderFilmsExtra(films, className) {

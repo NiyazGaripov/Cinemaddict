bbd7621502f8299361f647bf9fa10e11a97293e8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-11-24 22:33:32 +0300

    Transfers logic render film card in movie controller

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -1,7 +1,4 @@
 import {SortType} from './../constants';
-import {FilmCard} from '../components/film-card';
-import {onEscKeyDown} from '../utils/common';
-import {FilmInfo} from '../components/film-details';
 import {removeComponent, renderComponent} from '../utils/render';
 import {NoData} from '../components/no-data';
 import {Sort} from './../components/sort.js';
@@ -10,33 +7,6 @@ import {ShowMoreButton} from '../components/show-more-button';
 const FILM_CARDS_AMOUNT_ON_START = 5;
 const FILM_CARDS_AMOUNT_LOAD_MORE = 5;
 const BEGIN_INDEX = 0;
-
-const renderFilmCard = (filmsListContainer, filmCard) => {
-  const body = document.body;
-  const filmCardComponent = new FilmCard(filmCard);
-  const filmInfoComponent = new FilmInfo(filmCard);
-
-  const showFilmDetails = () => {
-    body.classList.add(`hide-overflow`);
-    body.appendChild(filmInfoComponent.getElement());
-    document.addEventListener(`keydown`, onDocumentEscKeyDown);
-  };
-
-  const hideFilmDetails = () => {
-    body.classList.remove(`hide-overflow`);
-    body.removeChild(filmInfoComponent.getElement());
-    document.removeEventListener(`keydown`, onDocumentEscKeyDown);
-  };
-
-  const onDocumentEscKeyDown = (evt) => {
-    onEscKeyDown(evt, hideFilmDetails);
-    document.removeEventListener(`keydown`, onDocumentEscKeyDown);
-  };
-
-  filmCardComponent.setClickHandler(showFilmDetails);
-  filmInfoComponent.setClickHandler(hideFilmDetails);
-  renderComponent(filmsListContainer, filmCardComponent);
-};
 
 const renderFilmCards = (cards, container) => {
   cards.forEach((card) => {
@@ -79,6 +49,8 @@ export class PageController {
     const parent = container.parentElement;
 
     const renderShowMoreButton = () => {
+      removeComponent(this._showMoreButton);
+
       if (showingFilmCards >= films.length) {
         return;
       }

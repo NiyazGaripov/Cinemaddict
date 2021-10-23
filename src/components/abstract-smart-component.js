2f38712dcd93e5e8c1816f3239947f4c39beb951
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-10-23 21:23:45 +0300

    feat: creates method recoveryListeners

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -1,5 +1,7 @@
 import {AbstractComponent} from "./abstract-component";
 
 export class AbstractSmartComponent extends AbstractComponent {
-
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
+  }
 }

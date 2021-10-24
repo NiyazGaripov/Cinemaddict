04e4629a25583d3972b56568c503d1578e47b221
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-10-24 18:26:22 +0300

    refactor: puts checked into constant and adds join

--- src/components/emoji-list.js
+++ src/components/emoji-list.js
@@ -2,13 +2,15 @@ import {EMOJIS} from './../constants.js';
 import {AbstractComponent} from "./abstract-component";
 
 const createEmojiListComponent = (currentEmoji) => EMOJIS.map((emoji) => {
+  const checked = emoji === currentEmoji ? `checked` : ``;
+
   return (
-    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}" ${currentEmoji ? `checked` : ``}>
+    `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}" ${checked}>
     <label class="film-details__emoji-label" for="emoji-${emoji}">
       <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="emoji-${emoji}">
     </label>`
   );
-});
+}).join(`\n`);
 
 export class Emoji extends AbstractComponent {
   constructor(currentEmoji) {

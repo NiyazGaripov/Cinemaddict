53ff1b6e64eeeac9da142569f6ac948159816068
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-03 17:35:27 +0300

    feat: creates method for remove comment

--- src/models/comments.js
+++ src/models/comments.js
@@ -12,4 +12,17 @@ export class Comments {
     this._comments = Array.from(comments);
     this._callHandlers(this._dataChangeHandlers);
   }
+
+  removeComment(id) {
+    const index = this._comments.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._comments = this._comments.filter((item) => item.id !== id);
+    this._callHandlers(this._dataChangeHandlers);
+
+    return true;
+  }
 }

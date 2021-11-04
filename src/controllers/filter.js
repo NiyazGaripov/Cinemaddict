f2c551bbcab8b68660c93b5edf08835e78907c52
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-04 18:59:12 +0300

    feat: creates class FilterController and adds constructor

---
+++ src/controllers/filter.js
@@ -0,0 +1,16 @@
+import {FilterType} from '../constants';
+
+export class FilterController {
+  constructor(container, filmsModel) {
+    this._container = container;
+    this._filmsModel = filmsModel;
+
+    this._activeFilterType = FilterType.ALL;
+    this._filterComponent = null;
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onFilterChange = this._onFilterChange.bind(this);
+
+    this._filmsModel.setDataChangeHandler(this._onDataChange);
+  }
+}

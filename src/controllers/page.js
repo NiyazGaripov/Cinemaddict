1d3fd8166ad24277c4603e9229ec4e2d126bc092
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-05 17:09:58 +0300

    feat: adds call method resetSortType when filter changed

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -171,6 +171,7 @@ export class PageController {
 
   _onFilterChange() {
     this._showingFilmCards = FILM_CARDS_AMOUNT_ON_START;
+    this._sortComponent.resetSortType();
     this._updateFilms(this._showingFilmCards);
   }
 

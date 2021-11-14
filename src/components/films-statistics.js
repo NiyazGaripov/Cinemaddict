78d5d32467cee9beb2d801b15fa58191b8070a24
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-14 20:11:06 +0300

    feat: creates function getTopGenre

--- src/components/films-statistics.js
+++ src/components/films-statistics.js
@@ -8,6 +8,32 @@ const getWatchedFilmsDuration = (watchedFilms) => {
   return watchedFilmsDurations.reduce((acc, it) => acc + parseFloat(it));
 };
 
+const getTopGenre = (watchedFilms) => {
+  let genresCount = {};
+
+  watchedFilms.map((film) => {
+    film.genres.map((genre) => {
+      if (genre in genresCount) {
+        genresCount[genre]++;
+      } else {
+        genresCount[genre] = 1;
+      }
+    });
+  });
+
+  let maxGenreCount = 1;
+  let topGenre = ``;
+
+  Object.keys(genresCount).map((genre) => {
+    if (maxGenreCount === 1 || genresCount[genre] > maxGenreCount) {
+      maxGenreCount = genresCount[genre];
+      topGenre = genre;
+    }
+  });
+
+  return topGenre;
+};
+
 const createStatisticComponent = () => {
   return (
     `<section class="statistic">

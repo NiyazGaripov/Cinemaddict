69bb22a194333ea3e865584bd6a5db0b73e4d686
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-14 20:05:28 +0300

    feat: creates func get user rank

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,5 @@
+import {UserRank} from '../constants';
+
 const MAX_VALUE = 10;
 
 export const getRandomIntegerNumber = (min, max) => {
@@ -21,4 +23,20 @@ export const setActiveClass = (container, element, activeClass) => {
     activeElement.classList.remove(activeClass);
     element.classList.add(activeClass);
   }
+};
+
+export const getUserRank = (amount) => {
+  let userRank;
+
+  if (amount === 0) {
+    return null;
+  } else if (amount >= 1 && amount <= 10) {
+    userRank = UserRank.NOVICE;
+  } else if (amount >= 11 && amount <= 20) {
+    userRank = UserRank.FAN;
+  } else if (amount >= 21) {
+    userRank = UserRank.MOVIE_BUFF;
+  }
+
+  return userRank;
 };

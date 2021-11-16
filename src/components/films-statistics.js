250a54b0046eb8d0a6e710a8a582fbf06cac313a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-16 19:39:36 +0300

    feat: imports DI and creates function renderChart

--- src/components/films-statistics.js
+++ src/components/films-statistics.js
@@ -1,4 +1,7 @@
 import {AbstractSmartComponent} from './abstract-smart-component';
+import {BAR_HEIGHT} from "../constants";
+import {Chart} from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 const getWatchedFilmsDuration = (watchedFilms) => {
   const watchedFilmsDurations = watchedFilms.map((film) => {
@@ -38,6 +41,69 @@ const getTopGenre = (watchedFilms) => {
   });
 
   return topGenre;
+};
+
+const renderChart = () => {
+  const statisticCtx = document.querySelector(`.statistic__chart`);
+
+  statisticCtx.height = BAR_HEIGHT * 5;
+
+  return new Chart(statisticCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`Sci-Fi`, `Animation`, `Fantasy`, `Comedy`, `TV Series`],
+      datasets: [{
+        data: [11, 8, 7, 4, 3],
+        backgroundColor: `#ffe800`,
+        hoverBackgroundColor: `#ffe800`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 20
+          },
+          color: `#ffffff`,
+          anchor: `start`,
+          align: `start`,
+          offset: 40,
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#ffffff`,
+            padding: 100,
+            fontSize: 20
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 24
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  });
 };
 
 const createStatisticComponent = () => {

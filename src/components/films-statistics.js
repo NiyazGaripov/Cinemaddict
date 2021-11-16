19bc758e1faca56d329faf1aa1eaf6ed33d560dd
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-16 19:41:04 +0300

    feat: creates function createStatisticRankComponent

--- src/components/films-statistics.js
+++ src/components/films-statistics.js
@@ -106,6 +106,16 @@ const renderChart = () => {
   });
 };
 
+const createStatisticRankComponent = (userRank) => {
+  return (
+    `<p class="statistic__rank">
+      Your rank
+      <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      <span class="statistic__rank-label">${userRank}</span>
+    </p>`
+  );
+};
+
 const createStatisticComponent = () => {
   return (
     `<section class="statistic">

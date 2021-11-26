ca82b4a9f384496c6d908482ea3d40c77388861b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2021-11-26 21:40:35 +0300

    feat: adds user rank in profile and constructor

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,20 +1,27 @@
-import {PROFILES} from '../constants';
-import {getRandomArrayItem} from '../utils/common';
 import {AbstractComponent} from './abstract-component';
+import {getUserRank} from '../utils/common';
+import {getWatchedFilms} from '../utils/filter';
 
-const createProfileComponent = () => {
-  const userRank = getRandomArrayItem(PROFILES);
+const createProfileComponent = (films) => {
+  const watchedFilms = getWatchedFilms(films);
+  const watchedFilmsAmount = watchedFilms.length;
+  const userRank = getUserRank(watchedFilmsAmount);
 
   return (
-    `<section class="header__profile profile">
+    userRank !== null ? `<section class="header__profile profile">
       <p class="profile__rating">${userRank}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    </section>`
+    </section>` : ``
   );
 };
 
 export class Profile extends AbstractComponent {
+  constructor(films) {
+    super();
+    this._films = films;
+  }
+
   getTemplate() {
-    return createProfileComponent();
+    return createProfileComponent(this._films);
   }
 }
